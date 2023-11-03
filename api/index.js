5f0c224e6cc30a97d0114eea0c0df148c8ea6096
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-03 13:26:33 +0530

    .env and mongoose connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express()
+console.log(process.env.MONGO);
 
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('Connected To Database successfully');
+}).catch((err)=>{
+console.log(err.message);
+})
 
 app.listen(3000,()=>{
   console.log('Server listening on port 3000');
