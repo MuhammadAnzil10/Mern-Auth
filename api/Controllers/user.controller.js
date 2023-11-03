ac995dbf793de79e94744cbd0a3ae1fa2aaac137
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-03 14:46:42 +0530

    create test api

---
+++ api/Controllers/user.controller.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+export const test =(req,res)=>{
+
+    res.json({
+      message:'Api is working'
+    })
+  
+}
--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 const app = express()
-console.log(process.env.MONGO);
+
 
 mongoose.connect(process.env.MONGO).then(()=>{
   console.log('Connected To Database successfully');
 }).catch((err)=>{
 console.log(err.message);
 })
+
+app.use('/',userRoutes)
+
+
+
 
 app.listen(3000,()=>{
   console.log('Server listening on port 3000');
