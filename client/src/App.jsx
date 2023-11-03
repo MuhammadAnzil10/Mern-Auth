74a65d6dc14b3386ade502934ee17cd2f85a8e57
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-03 11:49:22 +0530

    add routes and created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-Up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-const App =()=>{
-
-  return(
-    <div className='text-red-500' >App</div>
-  )
-}
-
-
-export default App
+export default App;
---
+++ client/src/pages/Signin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+const Signin=()=>{
+
+  return (
+    <div>
+      SignIn
+    </div>
+  )
+}
+
+
+export default Signin
