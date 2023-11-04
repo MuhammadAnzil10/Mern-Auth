cb6c2848d44d6e37d79ca2e6e998d4a568a72a80
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-04 12:15:50 +0530

    complete sign up page functionality

--- api/Controllers/auth.controller.js
+++ api/Controllers/auth.controller.js
@@ -3,11 +3,11 @@ import bcryptjs from 'bcryptjs'
 
 export const signup = async (req,res,next)=>{
 
-const {name,email,password} = req.body;
+const {username,email,password} = req.body;
 
 const hashedPassword = bcryptjs.hashSync(password,10)
 const newUser = new User({
-   username:name,
+    username,
     email,
     password:hashedPassword
 })
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,27 +1,90 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+const SignUp = () => {
+const [formData,setFormData]=useState({})
+const [error,setError] = useState(false)
+const [loading,setLoading] = useState(false)
 
-const SignUp =()=>{
+  const handleChange =(event)=>{
+     setFormData({...formData,[event.target.id]:event.target.value})
+  }
 
+  const handleSubmit =async(e)=>{
+    e.preventDefault()
+    try{
+      setLoading(true)
+      setError(false)
+      const response = await fetch(
+        'api/auth/signup',
+        {
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(formData)
+        }
+          )
+          const data = await response.json()
+           setLoading(false)
+           if(data.success === false){
+            setError(true)
+            return
+           }
+       
+    }catch(error){
+      
+     setLoading(false)
+     setError(true)
+
+    }
+    
+   
+       
+  }
+ 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form action="" className='flex flex-col gap-4'>
-        <input type="text" name="username" id="username" placeholder='Username'
-        className='bg-slate-100 p-3 rounded-lg' />
-        <input type="email" name="email" id="email" placeholder='Email'
-        className='bg-slate-100 p-3 rounded-lg' />
-        <input type="password" name="password" id="password" placeholder='Password'
-        className='bg-slate-100 p-3 rounded-lg' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+         {loading ? 'Loading...' : "Sign Up"}
+        </button>
       </form>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
-      <Link to='/sign-in'> <span className='text-blue-500'>Sign in</span></Link>
+        <Link to="/sign-in">
+          {" "}
+          <span className="text-blue-500">Sign in</span>
+        </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && 'Somthing went wrong'}</p>
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
