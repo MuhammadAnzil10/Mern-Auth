874a5e3ac04a5b21a5ee2fb2e9f6cf4430db4878
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-06 13:42:10 +0530

    complete home and about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,7 +5,29 @@ import React from "react";
 const About =()=>{
 
   return(
-    <div>About</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+    <h1 className="text-3xl font-bold mb-4 text-slate-700">
+      About
+    </h1>
+    <p className="mb-4 text-slate-800">
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only five
+      centuries, but also the leap into electronic typesetting, remaining
+      essentially unchanged. It was popularised in the 1960s with the release
+      of Letraset sheets containing Lorem Ipsum passages, and more recently
+      with desktop publishing software like Aldus PageMaker including versions
+      of Lorem Ipsum.
+    </p>
+    <p className="mb-4 text-slate-800">
+      There are many variations of passages of Lorem Ipsum available, but the
+      majority have suffered alteration in some form, by injected humour, or
+      randomised words which don't look even slightly believable. If you are
+      going to use a passage of Lorem Ipsum, you need to be sure there isn't
+      anything embarrassing hidden in the middle of text.
+    </p>
+  </div>
   )
 }
 
