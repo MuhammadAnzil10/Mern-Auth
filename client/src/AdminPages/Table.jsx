e4deee17e691d12de6d0a128031ab4d7a19bb120
Muhammad anzil <anzilanzil100@gmail.com>
2023-11-07 23:34:52 +0530

    add search functionality

--- client/src/AdminPages/Table.jsx
+++ client/src/AdminPages/Table.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import { set } from "mongoose";
+import React,{useState,useEffect} from "react";
+
 
 export default function Table({usersData}) {
-  console.log(usersData);
+  const [search,setSearch] = useState('')
+  const [filterUsers , setFilterUsers] = useState([])
+
+  useEffect(()=>{
+    setFilterUsers(usersData)
+  },[usersData])
+
+  const handleFilter =()=>{
+
+    const filtered = usersData.filter((user)=>{
+      const name = user.username.toLowerCase();
+      const email = user.email.toLowerCase();
+      const query = search.toLowerCase();
+      return name.includes(query) || email.includes(query);
+  
+    })
+  
+    setFilterUsers(filtered)
+  }
+ 
   return (
     
     <div className="container mx-auto p-4 pl-10 ">
       <h1 className="text-2xl font-semibold mb-4 pl-4">User Management</h1>
+      <div className="flex items-center">
+            <div className="flex border border-purple-200 rounded mb-8 mt-5">
+                <input
+                    type="text"
+                    className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={e=>setSearch(e.target.value)}
+                />
+                <button className="px-4 text-white bg-purple-600 border-l rounded "
+                onClick={handleFilter}>
+                    Search
+                </button>
+            </div>
+        </div>
+
+
+
+      
       <table className="min-w-full divide-y divide-gray-200 ">
         <thead>
           <tr>
@@ -26,7 +66,7 @@ export default function Table({usersData}) {
         <tbody className="bg-white divide-y divide-gray-200">
 
           {
-            usersData.map((user , index)=>{
+            filterUsers.map((user , index)=>{
 
           return ( <tr key={index}>
               <td className="px-6 py-4 whitespace-nowrap">{user.username}</td>
